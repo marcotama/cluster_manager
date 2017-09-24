6f622ded23d2c9349ea2945ff45d65ad0c8e5d22
Marco Tamassia <nazgul17@gmail.com>
2017-09-24 13:51:51 +1000

    Replace printing with logging and add support for proxy

--- cluster_manager.py
+++ cluster_manager.py
@@ -11,23 +11,25 @@ Extreme care is recommended to both commands and file paths passed: this script 
 you!
 
 """
-
 from collections import namedtuple
-import sys
-from queue import Queue # works if you have module future installed
+from queue import Queue  # works if you have the `future` module installed
 import random
 import os
-from thread_safe_file import ThreadSafeFile
 
 from joblib import Parallel, delayed
 
 # Used to establish ssh connections
 from paramiko.client import SSHClient
-from paramiko.sftp_client import SFTPClient
+from paramiko.config import SSHConfig
+from paramiko.proxy import ProxyCommand
 from paramiko import AutoAddPolicy
 
 # Used to prompt the password without echoing
 from getpass import getpass, getuser
+
+import logging
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO,
+                    datefmt='%a, %d %b %Y %H:%M:%S')
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -36,8 +38,8 @@ from getpass import getpass, getuser
 Host = namedtuple('Host', ['no_cpu', 'hostname', 'username', 'password', 'key_filename'], verbose=False)
 Job = namedtuple('Job', ['command', 'required_files', 'return_files', 'id'], verbose=False)
 TransferableFile = namedtuple('TransferableFile', ['local_path', 'remote_path'], verbose=False)
+ProgressData = namedtuple('ProgressData', ['no_total_jobs', 'no_finished_jobs'])
 
-stdout = ThreadSafeFile(sys.stdout)
 
 class ClusterManager:
     def __init__(self, hosts, jobs):
@@ -45,38 +47,64 @@ class ClusterManager:
         self.jobs = jobs  # type: 'List[Job]'
         self.workers = []  # type: 'List[SSHClient]'
         self.pool = Queue()  # type: 'Queue[SSHClient]'
+        self.progress_data = ProgressData(no_total_jobs=len(self.jobs), no_finished_jobs=0)
 
-        tot_workers = sum(host.no_cpu for host in hosts)
-        self.workers = Parallel(tot_workers, backend='threading')(delayed(create_worker)(host) for host in self.hosts for _ in range(host.no_cpu))
+        total_no_workers = sum(host.no_cpu for host in hosts)
+        self.workers = Parallel(total_no_workers, backend='threading')(delayed(create_worker)(host)
+                                                                       for host in self.hosts
+                                                                       for _ in range(host.no_cpu))
         for worker in self.workers:
             self.pool.put(worker)
 
+        logging.info("ABOUT TO RUN %d jobs in %d hosts (%d CPUs)" % (self.progress_data.no_total_jobs,
+                                                                     len(hosts), total_no_workers))
+
     def start(self):
-        results = Parallel(self.pool.qsize(), backend='threading')(delayed(run_job)(self.pool, job) for job in self.jobs)
+        results = Parallel(self.pool.qsize(), backend='threading')(delayed(run_job)(self.pool, job, self.progress_data)
+                                                                   for job in self.jobs)
         return results
 
     def start_single_threaded(self):
-        results = [run_job(self.pool, job) for job in self.jobs]
+        results = [run_job(self.pool, job, self.progress_data) for job in self.jobs]
         return results
 
+
 def create_worker(host):
+    config = SSHConfig()
+    proxy = None
+    if os.path.exists(os.path.expanduser('~/.ssh/config')):
+        with open(os.path.expanduser('~/.ssh/config')) as f:
+            config.parse(f)
+        if host.hostname is not None and 'proxycommand' in config.lookup(host.hostname):
+            proxy = ProxyCommand(config.lookup(host.hostname)['proxycommand'])
+
     worker = SSHClient()
     worker.load_system_host_keys()
     worker.set_missing_host_key_policy(AutoAddPolicy())
-    worker.connect(hostname=host.hostname, username=host.username, password=host.password, key_filename=host.key_filename)
+
+    # store data for later reference
+    worker.host = host.hostname
+    worker.username = host.username
+    worker.password = host.password
+    worker.key_filename = host.key_filename
+
+    worker.connect(hostname=host.hostname, username=host.username,
+                   password=host.password, key_filename=host.key_filename, sock=proxy)
     return worker
 
 
-def run_job(pool, job):
+def run_job(pool, job, progress_data):
     worker = pool.get()
     try:
-        run_job_on_worker(worker, job)
+        return run_job_on_worker(worker, job)
     finally:
         pool.put(worker)
+        progress_data.no_finished_jobs += 1
+        logging.info("Number of jobs finished so far: %d (out of %d)" % (progress_data.no_finished_jobs,
+                                                                         progress_data.no_total_jobs))
+
 
 def run_job_on_worker(worker, job):
-    stdout.write('Executing command: %s' % job.command)
-    stdout.write('\n')
 
     # create remote env
     instance_id = ''.join(random.choice('0123456789abcdef') for _ in range(30))
@@ -87,6 +115,8 @@ def run_job_on_worker(worker, job):
     for tf in job.required_files:
         sftp.put(localpath=tf.local_path, remotepath=tf.remote_path)
 
+    logging.info('About to execute command in host %s dir %s: %s \n' % (worker.host, dest_dir,  job.command))
+
     # run job
     actual_command = """cd %s ; sh -c '%s'""" % (dest_dir, job.command)
     _, ssh_stdout, ssh_stderr = worker.exec_command(actual_command, get_pty=True)  # Non-blocking call
@@ -94,7 +124,19 @@ def run_job_on_worker(worker, job):
 
     # retrieve results
     for tf in job.return_files:
-        sftp.get(localpath=tf.local_path, remotepath=tf.remote_path)
+        try:
+            sftp.get(localpath=tf.local_path, remotepath=tf.remote_path)
+        except Exception as e:
+            logging.error("\n \n Error copying remote file %s to local %s on %s: %s"
+                          % (tf.remote_path, tf.local_path, dest_dir, str(e)))
+            logging.info("Reconnecting to worker: %s \n\n" % worker.host)
+            worker.connect(hostname=worker.host, username=worker.username, password=worker.password,
+                           key_filename=worker.key_filename)
+
+    sftp.close()
+    # clean
+    worker.exec_command('rm -rf %s' % dest_dir)
+    logging.info('Successfully executed command in host %s dir %s: %s \n' % (worker.host, job.command, dest_dir))
 
     # clean
     worker.exec_command('rm -rf %s' % dest_dir)
@@ -124,7 +166,8 @@ if __name__ == '__main__':
         instance_id = ''.join(random.choice('0123456789abcdef') for _ in range(30))
         test_file = "%s.txt" % instance_id
 
-        command = "sleep 1; cat %s | head -1 > a.txt ; cat a.txt > %s ; ls -l >> %s ; echo ciao >> %s" % (test_file, test_file, test_file, test_file)
+        command = "sleep 1; cat %s | head -1 > a.txt ; cat a.txt > %s ; ls -l >> %s ; echo ciao >> %s" % \
+                  (test_file, test_file, test_file, test_file)
         req_file = TransferableFile(local_path='cluster_manager.py', remote_path=test_file)
         ret_file = TransferableFile(local_path=test_file, remote_path=test_file)
 
